606e2c9bf2aba3379a0f67561e435a911870614e
Cristhian Amaya <camaya@hugeinc.com>
2023-01-09 03:24:05 -0500

    botones

--- screens/battleground.js
+++ screens/battleground.js
@@ -1,5 +1,47 @@
-import { Text } from 'react-native';
+import { Text, Pressable, View, StyleSheet, ScrollView } from 'react-native';
+
 
 export default ({navigation, route}) => {
-    return <Text>This is your window Quichi</Text>;
+    return (
+        <View>
+        <Pressable style={styles.pressable}>
+        <Text style={styles.text}> Ascuas </Text>
+        </Pressable>
+        <Pressable style={styles.pressable}>
+        <Text style={styles.text}> Arañazo </Text>
+        </Pressable>
+        <Pressable style={styles.pressable}>
+        <Text style={styles.text}> Gruñido </Text>
+        </Pressable>
+        <Pressable style={styles.pressable}>
+        <Text style={styles.text}> Placaje </Text>
+        </Pressable>
+       
+        
+        </View>
+    )
 };
+    
+    const styles = StyleSheet.create({
+        text: {
+            color: 'white',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            marginTop: 'auto',
+            marginBottom: 'auto',
+            fontSize: 16,
+            fontWeight: '600'
+        },
+        wrapper: {
+          backgroundColor: "gray",
+          height: '100%'
+        },
+       
+        pressable: {
+          width: '100%',
+          height: '25%',
+          backgroundColor: "black",
+          fontSize: 50,
+        }
+      })
+    
--- screens/homepage.js
+++ screens/homepage.js
@@ -1,20 +1,47 @@
-import { Button } from 'react-native';
+import { Button, StyleSheet, Pressable} from 'react-native';
+
+import { Text, View, Image }  from 'react-native'  
+
 
 export default ({navigation}) => {
     return (
-      <>
-        <Button
-          title="This is Nico's Page"
-          onPress={() =>
-            navigation.navigate('ChoosePokemon', {name: 'Jane'})
-          }
-        />
-        <Button
-          title="This is Quichi's Page"
-          onPress={() =>
-            navigation.navigate('Battleground', {name: 'Jane'})
-          }
-        />
-      </>
+      <View style={styles.wrapper} >
+        <Image source={require('../img/pokemon.jpeg')} style={styles.image}/>
+        <Text style={styles.text}>Bienvenido a esta cosa c:</Text>
+        <Pressable  style={styles.pressable}>
+          <Text></Text>
+          <Button
+        title="Start"
+        onPress={() =>
+          navigation.navigate('Battleground', {name: 'Jane'})
+        }
+        ></Button>
+        </Pressable>
+      
+      </View>
     );
   };
+  
+  const styles = StyleSheet.create({
+    text: {
+      color: "blue",
+      fontSize: 80,
+    },
+    wrapper: {
+      backgroundColor: "gray",
+      height: '100%'
+    },
+    image: {
+      height: '40%',
+      width: '100%',
+
+    
+
+    },
+    pressable: {
+      width: '100%',
+      height: '8%',
+      backgroundColor: "white",
+      fontSize: 50,
+    }
+  })
